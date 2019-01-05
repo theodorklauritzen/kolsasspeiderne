af59a63c526a274d91daa30dd443271919919aa0
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2019-01-05 15:05:38 +0100

    small fixes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 let router = express.Router();
 
-router.get("/secret", function(req, res, next) {
-  res.send("This is secret, sort of");
-});
+// router.get("/secret", function(req, res, next) {
+//   res.send("This is secret, sort of");
+// });
 
 module.exports = router;
