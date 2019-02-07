7fc8f9155d532f5b85ab24b06d446b965e697866
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2019-02-07 14:28:13 +0100

    fixed some google oauth problems

--- routes/admin.js
+++ routes/admin.js
@@ -7,11 +7,13 @@ const { google } = require("googleapis")
 oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://127.0.0.1:3000/admin/login/callback"
+  "http://127.0.0.1:3000/admin/login/google/callback"
 );
 
 const oauth2Scopes = [
-  'https://www.googleapis.com/auth/plus.me'
+  //'https://www.googleapis.com/auth/plus.me'
+  'profile',
+  'email'
 ];
 
 const oauth2Url = oauth2Client.generateAuthUrl({
@@ -21,26 +23,43 @@ const oauth2Url = oauth2Client.generateAuthUrl({
 
 oauth2Client.on('tokens', (tokens) => {
   if (tokens.refresh_token) {
-    // store the refresh_token in my database!
+    // TODO: store the refresh_token in the database!
+    console.log("rt: ---------")
     console.log(tokens.refresh_token);
   }
+  console.log("at: ---------")
   console.log(tokens.access_token);
 });
 
+// mssql database configuration
+
 // redirect to Google oauth
 router.get("/login", (req, res, next) => {
-  //res.render("public/admin/login", {})
-  //res.send("preparing to redirect")
+  res.redirect("/admin/login/google")
+})
+
+router.get("/login/google", (req, res, next) => {
   res.redirect(oauth2Url)
 })
 
-router.get("/login/callback", async (req, res, next) => {
+router.get("/login/google/callback", async (req, res, next) => {
   res.send("Hola")
-  console.log(req)
-  const {tokens} = await oauth2Client.getToken(code)
-  oauth2Client.setCredentials(tokens);
+  const {tokens} = await oauth2Client.getToken(req.query.code)
+  oauth2Client.setCredentials(tokens)
 
-  console.log(tokens)
+  //console.log(tokens)
+
+  var oauth2 = google.oauth2({
+    auth: oauth2Client,
+    version: 'v2'
+  })
+  oauth2.userinfo.get((err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(res.data)
+    }
+  });
 })
 
 // Check if user is logged in
